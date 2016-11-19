69d49e1a3c52eeb1dbefbb642fda1e4125f3ce2f
anevmerj <anevmerj@sfu.ca>
2016-11-19 12:17:32 -0800

    jk
    kjj

---
+++ punGent/src/punGent/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package punGent;
+
+/**
+ * @author Alexei
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+
+	}
+
+}
