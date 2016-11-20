0865c0c0c2f2e5c3b3e9b0204af4c1d9b9f28eda
anevmerj <anevmerj@sfu.ca>
2016-11-19 16:21:15 -0800

    sutff
    stuff

--- src/pungent/PunGent.java
+++ src/pungent/PunGent.java
@@ -4,12 +4,24 @@
  * and open the template in the editor.
  */
 package pungent;
+import java.util.Vector;
+import java.util.Random;
 /**
  *
  * @author Scaret Camille Chao
  */
 public class PunGent {
-
+    
+     public static void main(String arg){
+         
+     }
+    
+String randomPunGenerator(Vector<String> collection){
+           Random rand = new Random();
+           int rnd_num = rand.nextInt() % PunGent.collection.size();
+           String rnd_pun = PunGent.collection.elementAt(randNumGen());
+           return rnd_pun;
+        }
     /**
      * @param args the command line arguments
      */
