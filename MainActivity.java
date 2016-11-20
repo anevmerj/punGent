4da9ce8db930466d4774da946d2fccd8a83455ab
shawlock <shawnalee1@shaw.ca>
2016-11-19 18:46:37 -0800

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.shawnalee.pungen;
 
-import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,29 +8,106 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Button;
 import android.view.Display;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.Reader;
 import java.lang.String;
 import android.widget.TextView;
 import java.util.Vector;
-import android.view.ViewGroup.LayoutParams;
+import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends PunGen{
+
 
     EditText input;
     LinearLayout mainLayout;
     Button pun_me;
     Button categories;
     Button ranpun;
-    TextView stringinput;
+    TextView output;
 
     Vector uservector;
 
+    Vector<catBase> collection = new Vector();
     int i = 0; //id for categories button
     int k = 0;
 
     String user_input;
 
     Display screen;
-    @Override
+
+        public void parser(){
+
+            String fileToRead = "PunGen.csv";
+            String sendToArray = null;
+            String[] stuff = new String[3];
+            String pun;
+            String cat;
+            String word;
+
+            try{
+                Reader readerFile = new FileReader(fileToRead);
+                BufferedReader br = new BufferedReader(readerFile);
+                while((sendToArray = br.readLine()) != null){
+                    stuff = sendToArray.split(",");
+                    pun = stuff[0];
+                    cat = stuff[1];
+                    word = stuff[2];
+                    if((pun.equals(""))){
+                        if(!(cat.equals(""))){
+                            collection.lastElement().add_cat(cat);
+                        }
+                        if(!(word.equals(""))){
+                            collection.lastElement().add_word(word);
+                        }
+
+                    }
+                    else{
+                        collection.addElement(new catBase(pun,cat,word));
+                    }
+
+                }
+
+            }catch(Exception err){
+                System.out.println(err);
+            }
+
+        }
+
+
+    boolean primaryErrorCheck(String user_input){
+        boolean errorFlag = true;
+        for(int i = 0; i < user_input.length(); i++)
+        {
+            if(!(Character.isLetter(user_input.charAt(i))))
+            {
+                errorFlag = false;
+            }
+        }
+        return errorFlag;
+    }
+
+    boolean Error_check(String arg, Vector<catBase> collection){
+        for(int i = 0;i<collection.size();i++){
+            if(collection.elementAt(i).is_word(arg)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Vector<String> punOut(String word, Vector<catBase> collection){
+        Vector<String> puns = new Vector();
+        for(int i = 0;i<collection.size();i++){
+            if(collection.elementAt(i).is_word(word)){
+                puns.add(collection.elementAt(i).get_pun());
+            }
+        }
+        return puns;
+    }
+
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
@@ -58,7 +134,8 @@ public class MainActivity extends AppCompatActivity {
 
         uservector = new Vector();
 
-        stringinput = new TextView(this);
+        output = new TextView(this);
+        mainLayout.addView(output);
 
         categories.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,20 +149,47 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        pun_me.setOnClickListener(new View.OnClickListener() {
+        /*pun_me.setOnClickListener(new View.OnClickListener() { //Needs to check error in user input. It will output either a pun or an error
             @Override
             public void onClick(View v) {
                 user_input = input.getText().toString();
-                uservector.add(user_input);
+            }
+        });*/
+            pun_me.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    boolean error1, error2;
+                    Vector<String> possiblePuns = new Vector();
+                    String randomPun;
 
-                stringinput.setText(uservector.get(k).toString());
-                mainLayout.addView(stringinput);
+                    user_input = input.getText().toString();
+                    //stringinput.setText(user_input);
 
-                //*************After implementation of string, clear the string*********
-                //user_input = "";
-                k++;
+                     user_input = user_input.toLowerCase();
+                    if(!(error1 = primaryErrorCheck(user_input))){
+                        randomPun = "Your input is not a word!";
+                    } else if(!(error2 = Error_check(user_input, collection))){
+                        randomPun = "Sorry, this word is not in our database.";
+                    } else {
+                        possiblePuns = punOut(user_input, collection);
+                        randomPun = randomPunGenerator(possiblePuns);
+                    }
+                    output.setText(randomPun);
+                }
+            });
+
+        ranpun.setOnClickListener(new View.OnClickListener() { //Output a pun
+            @Override
+            public void onClick(View v) {
+                Vector<String> puns = new Vector();
+                for(int i = 0;i<collection.size();i++){
+                        puns.add(collection.elementAt(i).get_pun());
+                }
+                output.setText(randomPunGenerator(puns));
+
             }
         });
+
         setContentView(mainLayout);
     }
 
