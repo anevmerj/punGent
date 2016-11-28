ea116b01adf263c1cb0f64e45d16b566a50942d7
shawlock <shawnalee1@shaw.ca>
2016-11-28 11:54:35 -0800

    Update catBase.java

--- src/pungent/catBase.java
+++ src/pungent/catBase.java
@@ -1,38 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package pungent;
+package com.example.shawnalee.pungen;
+
 import java.util.Vector;
 
 /**
  *
  * @author Alexei
  */
-public class catBase extends parser{
+public class catBase{
     private Vector<String> cat= new Vector();
     private String pun;
     private Vector<String> associated_words= new Vector();
-    
-    
-    catBase(String a, String b, String c){cat.add(a); pun =b;
-        associated_words.add(c);}
+
+
+    catBase(String a, String b, String c){cat.add(a); pun =b; associated_words.add(c);}
+
     public void add_word(String a){associated_words.addElement(a);}
+
     public void add_cat(String a){cat.addElement(a);}
+
     public String get_pun(){return pun;}
+
     public Vector<String> get_word(){
         return associated_words;
     }
+
     public Vector<String> get_cat(){return cat;}
-    
-    public boolean is_word(String arg){
+
+    public boolean is_word(String user_input){
         for(int i = 0; i < associated_words.size();i++){
-            if(arg.equals(associated_words.elementAt(i))){
+            if(user_input.equals(associated_words.elementAt(i))){
                 return true;
             }
-        }        
+        }
         return false;
     }
-    
+
 }
