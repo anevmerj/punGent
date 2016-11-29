c212f110cea47a1664f84485fffa4ace25861e8a
shawlock <shawnalee1@shaw.ca>
2016-11-29 11:00:26 -0800

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -259,14 +259,13 @@ public class MainActivity extends AppCompatActivity{
     void add_to_myPuns(String myPun){
         collection.addElement(new catBase(myPun));
         myPunsc4t.addElement(myPun);
-        FileOutputStream fos = null;
         try{
             FileOutputStream fos = openFileOutput("myPuns.txt", Context.MODE_APPEND);
             fos.write((myPun+",myPuns,").getBytes());
+            fos.close();
         }catch(IOException ex){
             ex.printStackTrace();
-        }finally{
-            fos.close();}            
+        }
     }
 
     @Override
