678b492336f8bac7308e6f311ae0d0498ac0a01a
sclchao <sclchao@gmail.com>
2017-03-15 08:58:11 -0700

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,37 +1,37 @@
 package com.example.mirna.pungent;
 
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.Gravity;
-import android.view.View;
 import android.content.Intent;
-import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.Button;
-import android.view.Display;
 import android.content.res.AssetManager;
 import android.content.Context;
 import android.graphics.Point;
 import android.graphics.Color;
 import android.graphics.Typeface;
-import android.widget.ImageView;
 import android.graphics.PorterDuff;
-import android.view.ViewGroup.LayoutParams;
 import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ViewGroup.LayoutParams;
+import android.view.Gravity;
+import android.view.Display;
+import android.view.ViewGroup;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Button;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.lang.String;
-import java.util.Vector;
-import java.util.Random;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.lang.String;
+import java.util.Vector;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity{
 
